4abc2eb2eee75c7d33989441605cbfe81e2e7221
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2021-02-20 16:07:00 +0100

    Added tests for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'rally-interview-recipe-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('rally-interview-recipe-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('rally-interview-recipe-app app is running!');
-  });
 });
